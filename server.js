dd2292f7b3a21e2c78cb3fa9fa3c5f0a92683ebd
Angic Carrasco <100764990+agcarrasco@users.noreply.github.com>
2024-04-18 00:11:04 +0800

    feat: added mongoose connection

--- server.js
+++ server.js
@@ -1,18 +1,37 @@
+require("dotenv").config();
+
 const express = require("express");
+const mongoose = require("mongoose");
+
 const v1Router = require("./routes/v1");
 
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME, PORT } = process.env;
+
+const initializeDb = async () => {
+  const uri = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authsource=admin`;
+  await mongoose.connect(uri);
+};
+
 const app = express();
-const PORT = 3000;
+const port = PORT || 3000;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+(async () => {
+  try {
+    await initializeDb();
 
-app.get("/", (req, res) => {
-  res.json({ message: "Simple Todo List API server is running" });
-});
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
 
-app.use("/api/v1", v1Router);
+    app.get("/", (req, res) => {
+      res.json({ message: "Simple Todo List API server is running" });
+    });
 
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
-});
+    app.use("/api/v1", v1Router);
+
+    app.listen(port, () => {
+      console.log(`Server is running at http://localhost:${port}`);
+    });
+  } catch (e) {
+    console.error(e);
+  }
+})();
